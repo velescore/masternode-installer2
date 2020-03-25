60f08631ebc771a51ea451806fc72fa32488efa0
AltcoinBaggins <marian@priv.profiweb.biz>
2020-03-25 18:20:43 +0100

    Refactor install.sh script

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+# Veles Core Masternode installation launcher (2nd gen, experimental)
+# Starts an installation process of the Veles Core Masternode
+
+if [[ "${1}" == "--help" ]]; then
+	echo "
+Veles Core Masternode installation launcher (2nd gen, experimental)
+ ____   ____     .__                _________                       
+_\___\_/___/____ |  |   ____   _____\_   ___ \  ___________   ____  
+\___________/__ \|  | _/ __ \ /  ___/    \  \/ /  _ \_  __ \_/ __ \ 
+   \  Y  /\  ___/|  |_\  ___/ \___ \\     \___(  <_> )  | \/\  ___/ 
+    \___/  \___  >____/\___  >____  >\______  /\____/|__|    \___  >
+               \/          \/     \/        \/                   \/ 
+
+Usage:  install.sh [options]
+Starts an installation process of the Veles Core Masternode
+
+Options:
+  --non-interactive         perform fully automatic installation
+  --docker-test             install from docker and run all tests
+"
+  exit 0;
+fi
+
 echo -e "Preparing Veles Masternode installation ...\n"
 echo -n "* Checking whether make command is present ... "
 
@@ -10,6 +33,7 @@ else
   apt-get install -yq make
 fi
 
+PWD=$(pwd)
 cd $(dirname $0)
 
 if [[ "${1}" == "--non-interactive" ]]; then
@@ -20,3 +44,4 @@ else
   make install
 fi
 
+cd $PWD
